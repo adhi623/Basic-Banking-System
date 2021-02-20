36f03c4e89d7593ac081d95d9cce240df6c6211b
Adhira Shashi <adhirashashi77@gmail.com>
2021-02-20 19:48:37 +0530

    MY WEBSITE

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+ <head>
+   <title>Infinity Bank</title>
+
+
+
+
+   <link rel="stylesheet" type="text/css" href="css/style.css">
+
+ </head>
+ <body>
+
+   <header>
+     <div class="main">
+
+       <ul>
+         <li class="active"><a href="">Home</a></li>
+         <li><a href="view.php">View Customers</a></li>
+          <li><a href="viewtransactions.php">View Transactions</a></li>
+
+       </ul>
+
+
+
+
+
+
+    <div class="title">
+      <h1>Welcome To Infinity Banks</h1>
+
+      </div>
+
+      <div class="title1">
+        <h3>We Make Money Transfer Easy!!</h3>
+      </div>
+
+      <div class="button">
+
+        <a href="transfer.php" class = "btn">Transfer</a>
+
+      </div>
+
+    </header>
+
+    </body>
+    </html>
+
+
+    <?php
+
+    include 'connection.php';
+
+    ?>
